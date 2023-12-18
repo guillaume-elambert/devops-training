a47973d015fa143945a00848e0dc7741373e21df
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-18 16:04:34 +0100

    Changed foreground script

--- ansible/00-understanding-infrastructure/assets/tools/foreground.sh
+++ ansible/00-understanding-infrastructure/assets/tools/foreground.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
-echo Waiting for all the setup scripts to finish...
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+UNDERLINE=$(tput smul)
 
 # Get all the /tmp/tools/*-setup.sh (except all-setup.sh) scripts in the current directory
 # Wait until there is no /tmp/tools/*-setup.lock file
-while [ -f /tmp/tools/*-setup.lock ]; do
-    sleep 1
+while [ -f /tmp/tools/*-setup.lock ];do 
+    for s in / - \\ \|; do 
+        printf "%s\r" "${YELLOW}${s} Waiting for all the setup scripts to finish${NORMAL}"
+        sleep 1
+    done
 done
-
-echo All setup scripts have finished. You can now enjoy the infrastructure.
+printf "%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"

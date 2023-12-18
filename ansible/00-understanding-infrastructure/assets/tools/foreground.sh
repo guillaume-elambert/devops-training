dd63555fd4ee3380510e7d2097f16ee35343edc7
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-18 17:10:27 +0100

    Changed time in loading foreground script

--- ansible/00-understanding-infrastructure/assets/tools/foreground.sh
+++ ansible/00-understanding-infrastructure/assets/tools/foreground.sh
@@ -11,10 +11,11 @@ UNDERLINE=$(tput smul)
 while [ -f /tmp/tools/*-setup.lock ];do 
     for s in / - \\ \|; do 
         printf "%s\r" "${YELLOW}${s} Waiting for all the setup scripts to finish...${NORMAL}"
-        sleep 1
+        sleep 0.5
     done
 done
 
 printf "%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"
 
 trap SIGINT
+touch /tmp/tools/t-setup.lock && sleep 5 && rm /tmp/tools/t-setup.lock & /tmp/tools/foreground.sh 

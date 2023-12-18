3bc8384453270c09e43f306d5310ce5f63004a7f
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-18 17:39:39 +0100

    Changed loading effect

--- ansible/00-understanding-infrastructure/assets/tools/foreground.sh
+++ ansible/00-understanding-infrastructure/assets/tools/foreground.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 trap '' SIGINT
+trap 'tput cnorm' EXIT
 
 NORMAL=$(tput sgr0)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 UNDERLINE=$(tput smul)
 
+tput civis
+
 # Get all the /tmp/tools/*-setup.sh (except all-setup.sh) scripts in the current directory
 # Wait until there is no /tmp/tools/*-setup.lock file
 while [ -f /tmp/tools/*-setup.lock ];do 
-    for s in / - \\ \|; do 
-        printf "%s\r" "${YELLOW}${s} Waiting for all the setup scripts to finish...${NORMAL}"
-        sleep 0.5
+    for s in ▖ ▘ ▝ ▗; do 
+        printf "\r%s" "${YELLOW}${s}\t Waiting for all the setup scripts to finish...${NORMAL}"
+        sleep 0.25
     done
 done
 
 printf "%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"
 
 trap SIGINT
-touch /tmp/tools/t-setup.lock && sleep 5 && rm /tmp/tools/t-setup.lock & /tmp/tools/foreground.sh 

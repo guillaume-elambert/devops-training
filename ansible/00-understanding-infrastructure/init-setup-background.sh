012d41040ae04bac0be66afc8babb39e4f1cf7cd
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-18 14:08:05 +0100

    Added foreground tasks

--- ansible/00-understanding-infrastructure/init-setup-background.sh
+++ ansible/00-understanding-infrastructure/init-setup-background.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Get all the *-setup.sh (except all-setup.sh) scripts in the current directory
+# Get all the /tmp/tools/*-setup.sh (except all-setup.sh) scripts in the current directory
 # and run them.
 for script in $(ls -1 /tmp/tools/*-setup.sh | grep -v all-setup.sh); do
     # Make the script executable
     chmod +x $script
-    echo "Running $script"
     
     # Remove the extension from the script name
     script_name=$(echo $script | sed 's/\(.*\)\..*/\1/')

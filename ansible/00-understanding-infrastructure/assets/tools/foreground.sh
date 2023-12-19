723b4d648d0ccddb236f27a988e7f3e9fba6e265
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-19 17:40:39 +0100

    Edited foreground scripts

--- ansible/00-understanding-infrastructure/assets/tools/foreground.sh
+++ ansible/00-understanding-infrastructure/assets/tools/foreground.sh
@@ -8,10 +8,11 @@ GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 UNDERLINE=$(tput smul)
 
-
 # Get all the /tmp/tools/*-setup.sh (except all-setup.sh) scripts in the current directory
 # Wait until there is no /tmp/tools/*-setup.lock file
 while [ -f /tmp/tools/*-setup.lock ];do 
+    had_lock=true
+
     for s in ▖ ▘ ▝ ▗; do 
         printf "\r%s\r" "${YELLOW}${s}  Waiting for all the setup scripts to finish...${NORMAL}"
         # remove any char that might be after the pritf above
@@ -19,6 +20,8 @@ while [ -f /tmp/tools/*-setup.lock ];do
     done
 done
 
-printf "\r%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"
+if [ "$had_lock" = true ]; then
+    printf "\r%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"
+fi
 
 trap SIGINT
--- ansible/00-understanding-infrastructure/setup/foreground.sh
+++ ansible/00-understanding-infrastructure/setup/foreground.sh
@@ -1,1 +1,5 @@
-clear && chmod +x /tmp/tools/foreground.sh && /tmp/tools/foreground.sh
+# Make /tmp/tools/foreground.sh executable
+# Force new terminal to run /tmp/tools/foreground.sh
+clear && \
+chmod +x /tmp/tools/foreground.sh && \
+echo /tmp/tools/foreground.sh >> ~/.bashrc

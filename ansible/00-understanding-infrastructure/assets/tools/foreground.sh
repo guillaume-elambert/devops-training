8ab58762cee4afff5aae053fb4ff264e5e382637
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-19 17:22:28 +0100

    Changed foreground.sh

--- ansible/00-understanding-infrastructure/assets/tools/foreground.sh
+++ ansible/00-understanding-infrastructure/assets/tools/foreground.sh
@@ -13,11 +13,12 @@ UNDERLINE=$(tput smul)
 # Wait until there is no /tmp/tools/*-setup.lock file
 while [ -f /tmp/tools/*-setup.lock ];do 
     for s in ▖ ▘ ▝ ▗; do 
-        printf "%s\r" "${YELLOW}${s}  Waiting for all the setup scripts to finish...${NORMAL}"
+        printf "\r%s\r" "${YELLOW}${s}  Waiting for all the setup scripts to finish...${NORMAL}"
+        # remove any char that might be after the pritf above
         sleep 0.25
     done
 done
 
-printf "%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"
+printf "\r%s\r\n" "${GREEN}All setup scripts have finished. You can now enjoy the infrastructure.${NORMAL}"
 
 trap SIGINT

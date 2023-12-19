0a21b6910e48080db05c287396fb87d69a1a6758
Guillaume ELAMBERT <guillaume.elambert@yahoo.fr>
2023-12-19 17:25:50 +0100

    Changed project-setup.sh

--- ansible/00-understanding-infrastructure/assets/tools/project-setup.sh
+++ ansible/00-understanding-infrastructure/assets/tools/project-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt update &&
-apt upgrade -y docker-compose &&
+# Create a docker network connected to the host
+docker network create ansible-host-network --driver host --attachable --scope local && \
 docker-compose -f "/root/ansible-training/docker-compose.yml" --compatibility up -d
